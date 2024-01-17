6fa8bc0f3a3f5efc1b63d2d57aab215cb44048a5
Brian Winkers <bwinkers@gmail.com>
2024-01-16 20:40:11 -0800

    Create script to create local certs

---
+++ scripts/create-local-certs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ./certs
+mkcert example.com api.example.com www.example.com
+mkcert com.example.com api.com.example.com www.com.example.com
+mkcert space.example.com api.space.example.com www.space.example.com
+mkcert shop.example.com api.shop.example.com www.shop.example.com
