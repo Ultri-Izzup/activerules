35f104b7a49ef67c4a19391ed05f74e953c52598
Brian Winkers <bwinkers@gmail.com>
2024-01-18 08:40:09 +0000

    WiP

--- scripts/production/create-certs.sh
+++ scripts/production/create-certs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo certbot certonly --nginx -d ultri.space -d api.ultri.space -d www.ultri.space
-sudo certbot certonly --nginx -d ultri.shop api.ultri.shop www.ultri.shop
-sudo certbot certonly --nginx -d ultri.com api.ultri.com www.ultri.com
+# sudo certbot certonly --nginx --dry-run -d ultri.space -d api.ultri.space -d www.ultri.space
+# sudo certbot certonly --nginx -d ultri.shop -d api.ultri.shop -d www.ultri.shop
+# sudo certbot certonly --nginx -d api.ultri.com -d www.ultri.com
+
+# docker compose -f ./infrastructure/production/certbot-compose.yaml run --rm  certbot certonly --dry-run --webroot --webroot-path /var/www/certbot/ -d ultri.space -d www.ultri.space -d api.ultri.space
+
